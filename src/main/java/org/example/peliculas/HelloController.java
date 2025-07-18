39453736f7dcfde90e3b502f2c54ca0853f3e26b
MireyaSilvano21 <20243ds132@utez.edu.mx>
2025-07-18 13:19:57 -0600

    Fución cargar ventana

--- src/main/java/org/example/peliculas/HelloController.java
+++ src/main/java/org/example/peliculas/HelloController.java
@@ -1,14 +1,44 @@
 package org.example.peliculas;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private AnchorPane contenedorCentral;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void mostararAñadir() throws IOException {
+        cargarVentana("añadirPelicula.fxml");
+    }
+
+    @FXML
+    private void mostrarPeliculas() throws IOException {
+        cargarVentana("listaPeliculas.fxml");
+    }
+
+    @FXML
+    private void mostrarEliminar() throws IOException {
+        cargarVentana("eliminarPeliculas.fxml");
+    }
+
+    @FXML
+    private void mostrarModificar() throws IOException {
+        cargarVentana("modificar.fxml");
+    }
+
+    @FXML
+    private void cargarVentana(String rutafxml) throws IOException {
+        AnchorPane vista = FXMLLoader.load(getClass().getResource(rutafxml));
+        AnchorPane.setTopAnchor(vista, 0.0);
+        AnchorPane.setBottomAnchor(vista, 0.0);
+        AnchorPane.setLeftAnchor(vista, 0.0);
+        AnchorPane.setRightAnchor(vista, 0.0);
+        contenedorCentral.getChildren().setAll(vista);
     }
 }
